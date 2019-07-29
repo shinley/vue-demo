10dccef2dd8967ab1bf708010d48e84ff6d5f741
shinley <cxl17258@qq.com>
2019-07-29 22:47:07 +0800

    添加模块

---
+++ src/router/index.js
@@ -0,0 +1,82 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+import Login from '../components/views/Login.vue'
+import Home from '../components/views/Home.vue'
+import Main from '../components/views/Main.vue'
+import NotFound from '../components/views/404.vue'
+import Table from '../components/views/nav1/Table.vue'
+import Form from '../components/views/nav1/Form.vue'
+import user from '../components/views/nav1/user.vue'
+import Page4 from '../components/views/nav2/Page4.vue'
+import Page5 from '../components/views/nav2/Page5.vue'
+import Page6 from '../components/views/nav3/Page6.vue'
+import echarts from '../components/views/charts/echarts.vue'
+
+let routers = [
+    {
+        path: '/login',
+        component: Login,
+        name: '',
+        hidden: true
+    },
+    {
+        path: '/404',
+        component: NotFound,
+        name: '',
+        hidden: true
+    },
+    //{ path: '/main', component: Main },
+    {
+        path: '/',
+        component: Home,
+        name: '导航一',
+        iconCls: 'el-icon-message',//图标样式class
+        children: [
+            { path: '/main', component: Main, name: '主页', hidden: true },
+            { path: '/table', component: Table, name: 'Table' },
+            { path: '/form', component: Form, name: 'Form' },
+            { path: '/user', component: user, name: '列表' },
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '导航二',
+        iconCls: 'fa fa-id-card-o',
+        children: [
+            { path: '/page4', component: Page4, name: '页面4' },
+            { path: '/page5', component: Page5, name: '页面5' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '',
+        iconCls: 'fa fa-address-card',
+        leaf: true,//只有一个节点
+        children: [
+            { path: '/page6', component: Page6, name: '导航三' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: 'Charts',
+        iconCls: 'fa fa-bar-chart',
+        children: [
+            { path: '/echarts', component: echarts, name: 'echarts' }
+        ]
+    },
+    {
+        path: '*',
+        hidden: true,
+        redirect: { path: '/404' }
+    }
+];
+
+export default new Router({
+    routers
+})
