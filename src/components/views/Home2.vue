980112072aed03f0acced2fd05bc57c59024234c
shinley <chenxl4@autoai.com>
2019-08-08 14:40:47 +0800

    修改content-main 的padding值

--- src/components/views/Home2.vue
+++ src/components/views/Home2.vue
@@ -3,7 +3,42 @@
         <div id="dHead">固定头部100px;</div>
         <div id="dBody">
             <div class="mycontent">
-                <div class="sidebar">sidebar</div>
+                <div class="sidebar">
+                    <el-menu
+                            default-active="2"
+                            class="el-menu-vertical-demo">
+                        <el-submenu index="1">
+                            <template slot="title">
+                                <i class="el-icon-location"></i>
+                                <span>导航一</span>
+                            </template>
+                            <el-menu-item-group>
+                                <template slot="title">分组一</template>
+                                <el-menu-item index="1-1">选项1</el-menu-item>
+                                <el-menu-item index="1-2">选项2</el-menu-item>
+                            </el-menu-item-group>
+                            <el-menu-item-group title="分组2">
+                                <el-menu-item index="1-3">选项3</el-menu-item>
+                            </el-menu-item-group>
+                            <el-submenu index="1-4">
+                                <template slot="title">选项4</template>
+                                <el-menu-item index="1-4-1">选项1</el-menu-item>
+                            </el-submenu>
+                        </el-submenu>
+                        <el-menu-item index="2">
+                            <i class="el-icon-menu"></i>
+                            <span slot="title">导航二</span>
+                        </el-menu-item>
+                        <el-menu-item index="3" disabled>
+                            <i class="el-icon-document"></i>
+                            <span slot="title">导航三</span>
+                        </el-menu-item>
+                        <el-menu-item index="4">
+                            <i class="el-icon-setting"></i>
+                            <span slot="title">导航四</span>
+                        </el-menu-item>
+                    </el-menu>
+                </div>
                 <div class="content">
                     <div class="content-top"></div>
                     <div class="content-main">
@@ -59,7 +94,7 @@
     }
 
     #dBody {
-        background: #FC0;
+        /*background: #FC0;*/
         width: 100%;
         overflow: hidden;
         top: 80px;
@@ -77,7 +112,8 @@
     .sidebar {
         flex: 0 0 200px;
         height: 100%;
-        background: #BCE8F1;
+        border-right: 1px #e6e6e6 solid;
+        /*background: #BCE8F1;*/
     }
 
     .content {
@@ -108,9 +144,12 @@
     }
     .content-main{
         position: absolute;
-        width: 100%;
+        width: calc(100% - 10px); /* 流下10px 给pading-left */
         top: 30px;
         bottom: 30px;
+        padding-top:10px;
+        padding-left:10px;
+        padding-bottom:10px;
         overflow: auto;
         z-index: 120;
     }
