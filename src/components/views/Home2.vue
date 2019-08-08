a910b7ba5fbd45be742bfc98fc4a4146437b6bd3
shinley <chenxl4@autoai.com>
2019-08-08 14:10:56 +0800

    修改布局

--- src/components/views/Home2.vue
+++ src/components/views/Home2.vue
@@ -3,44 +3,30 @@
         <div id="dHead">固定头部100px;</div>
         <div id="dBody">
             <div class="mycontent">
-                <div class="sidebar">
-                    <el-menu
-                            default-active="2"
-                            class="el-menu-vertical-demo">
-                        <el-submenu index="1">
-                            <template slot="title">
-                                <i class="el-icon-location"></i>
-                                <span>导航一</span>
-                            </template>
-                            <el-menu-item-group>
-                                <template slot="title">分组一</template>
-                                <el-menu-item index="1-1">选项1</el-menu-item>
-                                <el-menu-item index="1-2">选项2</el-menu-item>
-                            </el-menu-item-group>
-                            <el-menu-item-group title="分组2">
-                                <el-menu-item index="1-3">选项3</el-menu-item>
-                            </el-menu-item-group>
-                            <el-submenu index="1-4">
-                                <template slot="title">选项4</template>
-                                <el-menu-item index="1-4-1">选项1</el-menu-item>
-                            </el-submenu>
-                        </el-submenu>
-                        <el-menu-item index="2">
-                            <i class="el-icon-menu"></i>
-                            <span slot="title">导航二</span>
-                        </el-menu-item>
-                        <el-menu-item index="3" disabled>
-                            <i class="el-icon-document"></i>
-                            <span slot="title">导航三</span>
-                        </el-menu-item>
-                        <el-menu-item index="4">
-                            <i class="el-icon-setting"></i>
-                            <span slot="title">导航四</span>
-                        </el-menu-item>
-                    </el-menu>
-                </div>
+                <div class="sidebar">sidebar</div>
                 <div class="content">
-                    <router-view></router-view>
+                    <div class="content-top"></div>
+                    <div class="content-main">
+                        头部<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>
+                        内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>
+                        内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>
+                        内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>
+                        内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>
+                        内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>
+                        内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>
+                        内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>
+                        内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>
+                        内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>
+                        内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>
+                        内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>
+                        内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>
+                        内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>
+                        内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>
+                        内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>
+                        内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>
+                        内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>内容<br/>最后<br/>
+                    </div>
+                    <div class="content-bottom"></div>
                 </div>
             </div>
         </div>
@@ -62,8 +48,8 @@
     }
 
     #dHead {
-        height: 80px;
-        line-height: 80px;
+        height: 100px;
+        line-height: 100px;
         background: #690;
         width: 100%;
         position: absolute;
@@ -73,13 +59,13 @@
     }
 
     #dBody {
-        /*background: #FC0;*/
+        background: #FC0;
         width: 100%;
-        overflow: auto;
-        top: 80px;
+        overflow: hidden;
+        top: 100px;
         position: absolute;
         z-index: 10;
-        bottom: 50px;
+        bottom: 100px;
     }
 
     .mycontent {
@@ -91,26 +77,50 @@
     .sidebar {
         flex: 0 0 200px;
         height: 100%;
-        border-right: 1px #e6e6e6 solid;
-        /*background: #BCE8F1;*/
+        background: #BCE8F1;
     }
 
     .content {
+        position: relative;
         flex: 1;
         height: 100%;
-        /*background: #F0AD4E;*/
+        background: #F0AD4E;
         z-index: 110;
     }
 
     #dFoot {
-        height: 50px;
-        line-height: 50px;
+        height: 100px;
+        line-height: 100px;
         background: #690;
         width: 100%;
         position: absolute;
         z-index: 200;
         bottom: 0;
         text-align: center;
+    }
+    .content-top{
+        position: absolute;
+        top:0px;
+        width: 100%;
+        height: 30px;
+        background-color: aqua;
+        z-index: 120;
+    }
+    .content-main{
+        position: absolute;
+        width: 100%;
+        top: 30px;
+        bottom: 30px;
+        overflow: auto;
+        z-index: 120;
+    }
+    .content-bottom{
+        position: absolute;
+        bottom:0px;
+        width: 100%;
+        height: 30px;
+        background-color: aqua;
+        z-index: 120;
     }
     .el-menu {
         border-right: 0px;
