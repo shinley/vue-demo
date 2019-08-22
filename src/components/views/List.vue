85c874671889596b5f4e4eb3846049cbedb18bb0
shinley <chenxl4@autoai.com>
2019-08-22 14:04:03 +0800

    增加弹窗

--- src/components/views/List.vue
+++ src/components/views/List.vue
@@ -7,7 +7,7 @@
             </el-breadcrumb>
         </div>
         <div class="operation-menu">
-            <el-button type="primary" size="mini">新增部署</el-button>
+            <el-button type="primary" size="mini" @click="dialogFormVisible = true">新增部署</el-button>
         </div>
         <div class="content-main">
             <el-table
@@ -62,6 +62,24 @@
                     :total="total">
             </el-pagination>
         </div>
+        <!--dialog-->
+        <el-dialog title="收货地址" :modal-append-to-body='false' :visible.sync="dialogFormVisible" modal="true">
+            <el-form :model="form">
+                <el-form-item label="活动名称" :label-width="formLabelWidth">
+                    <el-input v-model="form.name" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="活动区域" :label-width="formLabelWidth">
+                    <el-select v-model="form.region" placeholder="请选择活动区域">
+                        <el-option label="区域一" value="shanghai"></el-option>
+                        <el-option label="区域二" value="beijing"></el-option>
+                    </el-select>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 
@@ -124,6 +142,18 @@
                 total: 0,
                 pageIndex: 1,
                 pageSize: 10,
+                dialogFormVisible: false,
+                form: {
+                    name: '',
+                    region: '',
+                    date1: '',
+                    date2: '',
+                    delivery: false,
+                    type: [],
+                    resource: '',
+                    desc: ''
+                },
+                formLabelWidth: '120px'
             }
         },
         mounted() {
